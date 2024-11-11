9bab4c7a362c2320086a08a157464de7a02253c0
Catasphorism <catasphorism@gmail.com>
2024-11-11 10:16:46 -0600

    Added Custom Item "Alejandroite"

--- src/main/java/net/catasphorism/tutorialmod/Config.java
+++ src/main/java/net/catasphorism/tutorialmod/Config.java
@@ -1,6 +1,5 @@
 package net.catasphorism.tutorialmod;
 
-import net.kaupenjoe.TutorialMod;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.ForgeConfigSpec;
@@ -37,7 +36,7 @@ public class Config
             .comment("A list of items to log on common setup.")
             .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
 
-    static final ForgeConfigSpec SPEC = BUILDER.build();
+    public static final ForgeConfigSpec SPEC = BUILDER.build();
 
     public static boolean logDirtBlock;
     public static int magicNumber;
